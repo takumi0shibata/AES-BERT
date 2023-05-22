b56f1030309f8954f1008f4ab2461a6ecde1a28a
Takumi Shibata <123294759+takumi0shibata@users.noreply.github.com>
2023-05-22 20:36:51 +0900

    Add files via upload

---
+++ utils/dataset.py
@@ -0,0 +1,33 @@
+from torch.utils.data import Dataset
+import torch
+
+class TextRegressionDataset(Dataset):
+    def __init__(self, texts, scores, tokenizer, max_length):
+        self.texts = texts
+        self.scores = scores
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, item):
+        text = str(self.texts[item])
+        score = self.scores[item]
+
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+
+        return {
+            'text': text,
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'score': torch.tensor(score, dtype=torch.float),
+        }
---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+def get_min_max_scores():
+    return {
+        1: {'Overall': (2, 12), 'Content': (1, 6), 'Organization': (1, 6), 'Word Choice': (1, 6),
+            'Sentence Fluency': (1, 6), 'Conventions': (1, 6)},
+        2: {'Overall': (1, 6), 'Content': (1, 6), 'Organization': (1, 6), 'Word Choice': (1, 6),
+            'Sentence Fluency': (1, 6), 'Conventions': (1, 6)},
+        3: {'Overall': (0, 3), 'Content': (0, 3), 'Prompt Adherence': (0, 3), 'Language': (0, 3), 'Narrativity': (0, 3)},
+        4: {'Overall': (0, 3), 'Content': (0, 3), 'Prompt Adherence': (0, 3), 'Language': (0, 3), 'Narrativity': (0, 3)},
+        5: {'Overall': (0, 4), 'Content': (0, 4), 'Prompt Adherence': (0, 4), 'Language': (0, 4), 'Narrativity': (0, 4)},
+        6: {'Overall': (0, 4), 'Content': (0, 4), 'Prompt Adherence': (0, 4), 'Language': (0, 4), 'Narrativity': (0, 4)},
+        7: {'Overall': (0, 30), 'Content': (0, 6), 'Organization': (0, 6), 'Conventions': (0, 6), 'Style': (0, 6)},
+        8: {'Overall': (0, 60), 'Content': (2, 12), 'Organization': (2, 12), 'Word Choice': (2, 12),
+            'Sentence Fluency': (2, 12), 'Conventions': (2, 12), 'Voice': (2, 12)}}
